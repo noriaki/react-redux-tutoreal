fb3bc96d16c505d83373ef9da4c840a6a95c5b8d
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2016-03-22 15:07:45 +0900

    add babel packages

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
         query: { presets: ['react', 'es2015'] }
       }
     ]
